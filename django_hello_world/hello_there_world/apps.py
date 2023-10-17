a138ffe702ed2ecdf0b8356071bbb78641e7a4f9
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-17 15:31:23 -0400

    initial commit.  huzzah

---
+++ django_hello_world/hello_there_world/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HelloThereWorldConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hello_there_world'
---
+++ django_hello_world/hello_there_world/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Greeting(models.Model):
+    message = models.CharField(max_length=256)
